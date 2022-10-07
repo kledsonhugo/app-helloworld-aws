fe424299f61589e164a4f6e22d478d2fba144457
Kledson Basso <kledsonhugo@gmail.com>
2022-10-07 18:27:17 -0300

    Provider Terraform file

--- terraform/hello-world.tf
+++ terraform/hello-world.tf
@@ -1,19 +1,3 @@
-# PROVIDER
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
-
-# REGION
-provider "aws" {
-    region = "us-east-1"
-    shared_credentials_file = ".aws/credentials"
-}
-
 # VPC
 resource "aws_vpc" "Hello_VPC" {
     cidr_block           = "10.0.0.0/16"
